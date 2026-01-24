7be7839bee0ff56b247226a31b240492e43722de
ahrenpradhan <ahrenpradhan@gmail.com>
2026-01-25 03:01:10 +0530

    init

---
+++ apps/api/server.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import Fastify from 'fastify'
+import mercurius from 'mercurius'
+
+const app = Fastify()
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async (_, { x, y }) => x + y
+  }
+}
+
+app.register(mercurius, {
+  schema,
+  resolvers
+})
+
+app.get('/', async function (_req, reply) {
+  const query = '{ add(x: 2, y: 2) }'
+  return reply.graphql(query)
+})
+
+app.get('/health', async () => ({ ok: true }));
+
+app.listen({ port: 3000 })
